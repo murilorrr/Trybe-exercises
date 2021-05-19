cde07fbccf7ed966edd0e8d8655f84eecc34cbd6
Murilo Ribeiro <murilorsv14@gmail.com>
2021-05-19 16:19:22 -0300

    os 5 primeiros programas juntos como funçoes

---
+++ bloco4/dia-1/javascript.js
@@ -0,0 +1,46 @@
+// Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+// Adição (a + b)
+// Subtração (a - b)
+// Multiplicação (a * b)
+// Divisão (a / b)
+// Módulo (a % b)
+
+// // -----------------------------------
+
+let a = 0
+let b = 0
+function adicao(a,b){
+    let c = a + b
+    console.log(c)
+}
+
+function subtracao(a,b){
+    var c = a - b
+   console.log(c)
+}
+
+function multiplicacaoo(a,b){
+    var c = a * b
+    console.log(c)
+}
+
+function divisao(a,b){
+    let c = a / b
+}
+
+function adição(a,b){
+    var c = a + b
+    console.log(c)
+}
+
+function modulo(a,b){
+    var c = a % b
+    console.log(c)
+}
+a = 3
+b = 2
+adicao(a,b)
+subtracao(a,b)
+multiplicacaoo(a,b)
+divisao(a,b)
+modulo(a,b)
