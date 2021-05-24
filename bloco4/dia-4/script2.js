f8379c941a1a8416035a25500a3e71f99bae8ab8
Murilo Ribeiro <murilorsv14@gmail.com>
2021-05-24 15:22:30 -0300

    exercicio do sedan

---
+++ bloco4/dia-4/script2.js
@@ -0,0 +1,16 @@
+let pizza = {
+    sabor: "catupiry",
+    preco: 39.90,
+    borda: true
+}
+
+
+
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+for (let key in car){
+  console.log(key, car[key])
+}
