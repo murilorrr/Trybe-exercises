c3c0cf775bd5387fcc16177616d51f3a27a6d58f
Murilo Ribeiro <murilorsv14@gmail.com>
2021-07-07 16:08:20 -0300

    exercicios do dia

---
+++ exercices/bloco-9/dia-1/javascript_exercises.js
@@ -0,0 +1,16 @@
+const myExpenses = [
+  {
+    gym: 99,
+  },
+  {
+    ifood: 200,
+  },
+  {
+    phone: 60,
+  },
+  {
+    internet: 100,
+  },
+];
+
+const myIncome = 1000;
---
+++ exercices/bloco-9/dia-1/script.js
@@ -0,0 +1,17 @@
+// apiScript.js     
+const API_URL = 'https://icanhazdadjoke.com/';
+
+const fetchJoke = () => {
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+
+  fetch(API_URL, myObject)
+    .then(response => response.json())
+    .then(data => console.log(data));
+};
+const c = document.createElement('div');
+c.innerText = fetchJoke();
+document.body.appendChild(c);
+window.onload = () => fetchJoke();
