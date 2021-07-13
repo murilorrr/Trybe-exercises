ab7c685ab3094ae129bf897839af52953af82d53
Murilo Ribeiro <murilorsv14@gmail.com>
2021-07-13 13:11:35 -0300

    exercicios do dia

--- exercices/bloco-9/dia-1/javascript_exercises.js
+++ exercices/bloco-9/dia-1/javascript_exercises.js
@@ -14,3 +14,22 @@ const myExpenses = [
 ];
 
 const myIncome = 1000;
+
+const monthlyBudget = (myIncome, myExpenses, callback) => {
+
+  const totalExpenses = callback(myExpenses);
+  const totalAfterExpenses = myIncome - totalExpenses;
+
+  console.log(`Balanço do mês:
+    Recebido: R$${myIncome},00
+    Gasto: R$${totalExpenses},00
+    Saldo: R$${totalAfterExpenses},00 `);
+};
+
+const handleExpenses = myExpenses => {
+  const eachValue = myExpenses.map((item) => Object.values(item)[0]);
+  const totalExpense = eachValue.reduce((acc, curr) => acc + curr , 0);
+  return totalExpense;
+};
+
+monthlyBudget(myIncome, myExpenses, handleExpenses);
