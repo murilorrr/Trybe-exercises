bc3cd125d67e81e1be52b9167ac419b2abc94420
Murilo Ribeiro <murilorsv14@gmail.com>
2022-03-21 15:45:02 -0300

    test: adiciona teste para numeros pares

--- ComputerScience/bloco33/dia_3/codigo.py
+++ ComputerScience/bloco33/dia_3/codigo.py
@@ -1,3 +1,8 @@
 def is_odd(number):
-    'Retorna True se um número é ímpar, senão False.'
+    "Retorna True se um número é ímpar, senão False."
     return number % 2 != 0
+
+
+def is_even(number):
+    "Retorna True se um numero é par, senão False."
+    return number % 2 == 0
---
+++ ComputerScience/bloco33/dia_3/test_codigo.py
@@ -0,0 +1,11 @@
+from codigo import is_odd, is_even
+
+
+def test_is_odd():
+    "para numero 3"
+    assert is_odd(3) is True
+
+
+def test_is_even():
+    "para numero 2"
+    assert is_even(2) is True
