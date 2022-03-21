1857c4d23d4bcc0a1b034daea07151ff1893cdff
Murilo Ribeiro <murilorsv14@gmail.com>
2022-03-21 15:46:48 -0300

    adiciona casos de erro

--- ComputerScience/bloco33/dia_3/test_codigo.py
+++ ComputerScience/bloco33/dia_3/test_codigo.py
@@ -1,11 +1,21 @@
-from codigo import is_odd, is_even
+import codigo
 
 
-def test_is_odd():
-    "para numero 3"
-    assert is_odd(3) is True
+def test_is_odd_when_number_is_odd_returns_true():
+    "Para um número ímpar a função deve retornar o valor True"
+    assert codigo.is_odd(3) is True
 
 
-def test_is_even():
-    "para numero 2"
-    assert is_even(2) is True
+def test_is_odd_when_number_is_even_returns_false():
+    "Para um número par a função deve retornar o valor False"
+    assert codigo.is_odd(2) is False
+
+
+def test_is_even_when_number_is_odd_returns_false():
+    "Para um número ímpar a função deve retornar o valor True"
+    assert codigo.is_even(3) is False
+
+
+def test_is_even_when_number_is_even_returns_true():
+    "Para um número par a função deve retornar o valor False"
+    assert codigo.is_even(2) is True
