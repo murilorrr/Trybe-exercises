160404475ed9e2c162d2cade222d04911ad77de1
Murilo Ribeiro <murilorsv14@gmail.com>
2022-04-07 16:02:26 -0300

    segundo crawler

--- ComputerScience/bloco35/dia_3/crawler/crawler.py
+++ ComputerScience/bloco35/dia_3/crawler/crawler.py
@@ -5,4 +5,5 @@ import requests
 response = requests.get("http://books.toscrape.com/")
 selector = Selector(text=response.text)
 titulos = selector.css("h3 a::attr(title)").getall()
+precos = selector.css(".product_price .price_color::text").getall()
 print(titulos)
