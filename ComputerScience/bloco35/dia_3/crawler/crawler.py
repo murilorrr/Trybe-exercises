c458a808aeb9b8d71851556f9435fb71e29bc471
Murilo Ribeiro <murilorsv14@gmail.com>
2022-04-07 16:12:48 -0300

    precos com formatação US

--- ComputerScience/bloco35/dia_3/crawler/crawler.py
+++ ComputerScience/bloco35/dia_3/crawler/crawler.py
@@ -1,9 +1,11 @@
 from parsel import Selector
 import requests
-
+import locale
+locale.setlocale(locale.LC_MONETARY, "en_US.UTF-8")
 
 response = requests.get("http://books.toscrape.com/")
 selector = Selector(text=response.text)
 titulos = selector.css("h3 a::attr(title)").getall()
 precos = selector.css(".product_price .price_color::text").getall()
-print(titulos)
+preco = [locale.currency(float(n.split("£")[1])) for n in precos]
+print(preco)
